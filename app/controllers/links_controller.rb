20a3c002a4f0a1ee90f31c2735ef53c5990f8c38
Dean Hildebrand <dean.hildebrand88@gmail.com>
2026-09-09 11:18:54 +0200

    url migration, basic controller, model and index view setup

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,22 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def new; end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path, notice: "Link was successfully created."
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
+end
