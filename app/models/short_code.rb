d7f3f00eb5cd8fcb164c4cc35aab7401b62b125f
Dean Hildebrand <dean.hildebrand88@gmail.com>
2026-09-12 13:27:38 +0200

    base62 decoding && tests

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -23,6 +23,27 @@ class ShortCode
     result
   end
 
+  # number = 0
+  # string = "4c91"
+  # number = 0 * 62 + ALPHABET.index("4") = 4
+  # number = 4 * 62 + ALPHABET.index("c")
+  # number = 4 * 62 + 12 = 260
+
+  # number = 260 * 62 + ALPHABET.index("9")
+  # number = 260 * 62 + 9 = 16129
+
+  # number = 16129 * 62 + ALPHABET.index("1")
+  # number = 16129 * 62 + 1 = 999_999
+
   def self.decode(string)
+    return 0 if string.nil? || string.empty?
+
+    number = 0
+
+    string.each_char do |char|
+      number = number * BASE + ALPHABET.index(char)
+    end
+
+    number
   end
 end
